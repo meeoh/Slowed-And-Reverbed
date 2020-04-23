c234986f6322eef7a3824fac4260f529de125709
Shameel Abdullah <shameel.abdullah@gmail.com>
2020-04-23 10:33:58 -0400

    Cleanup youtube uploading a bit

--- Youtube.py
+++ Youtube.py
@@ -9,24 +9,27 @@ from googleapiclient.http import MediaFileUpload
 
 scopes = ["https://www.googleapis.com/auth/youtube.upload"]
 
+
+# Code to generate the required token.json, in order to run this you need client_secrets.json from youtubes api
+# client_secrets_file = "client_secrets.json"
+# flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+#     client_secrets_file, scopes)
+# credentials = flow.run_console()
+
+# creds_data = {
+#   'token': credentials.token,
+#   'refresh_token': credentials.refresh_token,
+#   'token_uri': credentials.token_uri,
+#   'client_id': credentials.client_id,
+#   'client_secret': credentials.client_secret,
+#   'scopes': credentials.scopes
+# }
+# del creds_data['token']
+# with open('token.json', 'w') as outfile:
+#     json.dump(creds_data, outfile)
+
+
 def getCreds():
-    # Get credentials and create an API client
-    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-    #     client_secrets_file, scopes)
-    # credentials = flow.run_console()
-
-    # creds_data = {
-    #   'token': credentials.token,
-    #   'refresh_token': credentials.refresh_token,
-    #   'token_uri': credentials.token_uri,
-    #   'client_id': credentials.client_id,
-    #   'client_secret': credentials.client_secret,
-    #   'scopes': credentials.scopes
-    # }
-    # del creds_data['token']
-    # with open('token.json', 'w') as outfile:
-    #     json.dump(creds_data, outfile)
-
   with open('token.json') as json_file:
     credentials = json.load(json_file)
   credentials = google.oauth2.credentials.Credentials.from_authorized_user_file('./token.json')
@@ -34,13 +37,15 @@ def getCreds():
 
 
 def upload(path, options):
+    if not os.path.exists("token.json"):
+      print("token.json does not exist, not uploading to youtube")
+      return
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "client_secrets.json"
 
     credentials = getCreds()
     youtube = googleapiclient.discovery.build(
