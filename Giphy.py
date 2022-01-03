8c5df7b5cc14d0711434e0610adb3e696b2a2ae1
Shameel Abdullah <shameel.abdullah@gmail.com>
2022-01-03 15:29:14 -0500

    Update giphy api

--- Giphy.py
+++ Giphy.py
@@ -1,8 +1,10 @@
 import requests
 import os
 
-def download_gif(path='downloads/'):
+def download_gif(path='downloads/temp.gif'):
   response = requests.get(f"https://api.giphy.com/v1/gifs/random?api_key={os.getenv('GIPHY_API_KEY')}&tag=anime")
-  uri = response.json()['data']['image_url']
+  uri = response.json()['data']['images']['original']['url']
   with open(path, 'wb') as f:
     f.write(requests.get(uri).content)
+
+download_gif('./test.gif')
