b0c285647136c0dccb65e176294a8aa4597075da
Shameel Abdullah <shameel.abdullah@gmail.com>
2022-01-03 15:35:21 -0500

    Remove extra call

--- Giphy.py
+++ Giphy.py
@@ -3,8 +3,7 @@ import os
 
 def download_gif(path='downloads/temp.gif'):
   response = requests.get(f"https://api.giphy.com/v1/gifs/random?api_key={os.getenv('GIPHY_API_KEY')}&tag=anime")
+  print(response)
   uri = response.json()['data']['images']['original']['url']
   with open(path, 'wb') as f:
     f.write(requests.get(uri).content)
-
-download_gif('./test.gif')
